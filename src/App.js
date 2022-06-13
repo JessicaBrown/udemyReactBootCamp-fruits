f844b6c91cb5d88617fd27cbab464fa4b2a5229f
Jess <jessica.brown@tbsokc.com>
2022-06-13 11:17:57 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+import foods from './foods';
+import { choice, remove } from './helpers';
+
 function App() {
+  let fruit = choice(foods);
+  let remaining = remove(foods, fruit);
+  console.log(fruit)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Slot Machines!</h1>
+      <p>I’d like a fruit that looks like this {fruit.img}, please.</p>
+      <p>Here is one {fruit.name}</p>
+      <p>'Delicious! May I have another?'</p>
+      <p>I’m sorry, we’re all out. We have {remaining.length} other fruit left to choose from.</p> 
     </div>
   );
 }
 
 export default App;
+
---
+++ src/foods.js
@@ -0,0 +1,9 @@
+const fruits =[
+    {img: "🍇", name: 'Rasberry'},{img: "🍈", name: 'Lime'} ,{img: "🍉", name: 'Watermelon'} ,{img: "🍊", name: 'Peach'} , {img:  "🍋", name: 'Lemon'},{img: "🍌", name: 'Bananana'},{img: "🍍", name: 'Pinapple'},{img:  "🍎", name: 'Apple'}
+    ,{img:  "🍏", name: 'Green Apple'},{img:  "🍐", name: 'Pear'},{img:  "🍒", name: 'Cherry'},{img:  "🍓", name: 'Strawberry'},{img:  "🥝", name: 'Kiwi'},{img:  "🍅", name: 'Tomatoe'},{img:  "🥑", name: 'Avacado'}
+];
+//   const fruits =[
+//     "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🍎",
+//     "🍏", "🍐", "🍒", "🍓", "🥝", "🍅", "🥑",
+//   ];
+  export default fruits;
